4215cb2e0763b687077ffa8c392383750fc345fb
Miquel GB <mgines@bbsoftbalear.com>
2020-03-04 15:28:44 +0100

    Added QuickSort and MergeSort

--- sorting-viewer/src/app/app.component.ts
+++ sorting-viewer/src/app/app.component.ts
@@ -9,18 +9,38 @@ import { Subject } from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
+   /**
+    * Values list to sort
+    */
    public values: number[] = [];
+   /**
+    * Amount of values to generate into the values list
+    */
+   public valuesAmount: number = 10; // If entered a value higher than 175 the bars start to be hard to see
+   /**
+    * Flag to check if the list is already sorted and disable buttons
+    */
+   public sorted: boolean = false;
 
-   public valuesAmount: number = 50;
-
+   /**
+    * Default constructor
+    * 
+    * @param _sortService referenc to the Sort Service, which contains sorting methods
+    */
    constructor(private _sortService: SortService) {
 
    }
 
+   /**
+    * Angular OnInit lifecycle hook, calls the random values list generator
+    */
    public ngOnInit() {
       this.generateValues();
    }
 
+   /**
+    * Generates an amount equal to valuesAmount of random integers from 1-1000 into the values list
+    */
    public generateValues() {
       this.values = [];
       for (let i = 0; i < this.valuesAmount; i++) {
@@ -28,18 +48,66 @@ export class AppComponent implements OnInit {
       }
    }
 
+   /**
+    * Returns a random integer between 1 and 1000
+    */
    private _getRndInt() {
       return Math.floor(Math.random() * (1000 - 1)) + 1;
    }
 
-   public sort() {
-      this._sortService.bubbleSort(this.values);
+   /**
+    * Handles the sorting method clicked
+    * 
+    * @param sortMethod Sorting method selected
+    */
+   public sort(sortMethod: number = 0) {
+      switch (sortMethod) {
+         case 0:
+            this._sortService.bubbleSort(this.values);
+            break;
+         case 1:
+            this._sortService.quicksort(this.values);
+            break;
+            case 2:
+            this._sortService.mergeSort(this.values);
+            break;
+         default:
+            this._sortService.bubbleSort(this.values);
+            break;
+      }
+      //this._startTimer();
+      this.sorted = true;
    }
 
+   public timeLeft: number = 60;
+   private _interval;
+
+   private _startTimer() {
+      this._interval = setInterval(() => {
+         if (this.timeLeft > 0) {
+            this.timeLeft--;
+         } else {
+            this.timeLeft = 60;
+         }
+      }, 1000);
+   }
+
+   private _pauseTimer() {
+      clearInterval(this._interval);
+   }
+
+   /**
+    * Creates a new values list
+    */
    public reset() {
       this.generateValues();
+      //this._pauseTimer();
+      this.sorted = false;
    }
 
+   /**
+    * NOT WORKING YET
+    */
    public changeAmount() {
       this._sortService.changeWidthSubject.next(this.valuesAmount);
       this.generateValues();
--- sorting-viewer/src/app/services/sort.service.ts
+++ sorting-viewer/src/app/services/sort.service.ts
@@ -8,7 +8,7 @@ export class SortService {
 
    public speed: number = 100;
 
-  public changeWidthSubject: Subject<number> = new Subject<number>();
+   public changeWidthSubject: Subject<number> = new Subject<number>();
 
    constructor() { }
 
@@ -20,8 +20,52 @@ export class SortService {
     * @param list List of items to sort
     */
    public quicksort(list: number[]) {
-
+      this._quicksort(list, 0, list.length - 1);
    }
+
+
+   private _quicksort(list: number[], low: number, high: number) {
+      if (low < high) {
+         setTimeout(
+            () => {
+               /* pi is partitioning index, list[pi] is 
+                        now at right place */
+               let pi = this.partition(list, low, high);
+
+               // Recursively sort elements before 
+               // partition and after partition 
+               this._quicksort(list, low, pi - 1);
+               this._quicksort(list, pi + 1, high);
+            }, (1)
+         );
+      }
+   }
+
+
+   private partition(list: number[], low: number, high: number): number {
+      let pivot: number = list[high];
+      let i: number = (low - 1); // index of smaller element 
+      for (let j: number = low; j <= high - 1; j++) {
+         // If current element is smaller than or 
+         // equal to pivot 
+         if (list[j] <= pivot) {
+            i++;
+            // swap list[i] and list[j] 
+            let temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+         }
+      }
+
+      // swap list[i+1] and list[high] (or pivot) 
+      let temp = list[i + 1];
+      list[i + 1] = list[high];
+      list[high] = temp;
+
+      return i + 1;
+   }
+
+
 
    /* BUBBLE SORT */
 
@@ -42,10 +86,61 @@ export class SortService {
                      list[j] = list[j + 1];
                      list[j + 1] = temp;
                   }
-               }, (i * this.speed + j * this.speed)
-            )
+               }, (1)
+            );
          }
 
       }
    }
+
+   /* MERGE SORT */
+
+   public mergeSort(list: number[]) {
+      // const myClonedArray  = Object.assign([], list);
+      // list = this._mergeSort(myClonedArray);
+      // console.log(this._mergeSort(myClonedArray));
+   }
+
+   // Merge Sort Implentation (Recursion)
+   private _mergeSort(unsortedArray) {
+      // No need to sort the array if the array only has one element or empty
+      if (unsortedArray.length <= 1) {
+         return unsortedArray;
+      }
+      // In order to divide the array in half, we need to figure out the middle
+      const middle = Math.floor(unsortedArray.length / 2);
+
+      // This is where we will be dividing the array into left and right
+      const left = unsortedArray.slice(0, middle);
+      const right = unsortedArray.slice(middle);
+
+      let a = this.merge(
+         this._mergeSort(left), this._mergeSort(right)
+      );
+      // Using recursion to combine the left and right
+      return a;
+   }
+
+   // Merge the two arrays: left and right
+   public merge(left, right) {
+      let resultArray = [], leftIndex = 0, rightIndex = 0;
+
+      // We will concatenate values into the resultArray in order
+      while (leftIndex < left.length && rightIndex < right.length) {
+         if (left[leftIndex] < right[rightIndex]) {
+            resultArray.push(left[leftIndex]);
+            leftIndex++; // move left array cursor
+         } else {
+            resultArray.push(right[rightIndex]);
+            rightIndex++; // move right array cursor
+         }
+      }
+
+      // We need to concat here because there will be one element remaining
+      // from either left OR the right
+      return resultArray
+         .concat(left.slice(leftIndex))
+         .concat(right.slice(rightIndex));
+   }
+
 }
